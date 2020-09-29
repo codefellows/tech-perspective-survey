90be3fa6f0c9815c044d2f5e2540eecde0eb086e
paul-leonard <paul.ryan.leonard@gmail.com>
2020-09-28 20:48:29 -0700

    created schema and seed files for postgres database

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO survey_results (survey_instance, results) VALUES ('seattle-301d66','[4,7,7,9,11,12,12,13,14,14,14,15,18]');
+
+INSERT INTO survey_results (survey_instance, results) VALUES ('seattle-301d66','[3,7,8,9,10,11,13,13,14,14,14,15,17]');
+
+INSERT INTO survey_results (survey_instance, results) VALUES ('seattle-301d66','[6,7,7,9,9,10,11,11,13,14,16,17,19]');

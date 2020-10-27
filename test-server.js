54da545d3c7b1f7d8821686a28bfe53d94dfc759
Mark Bell <skrambelled@gmail.com>
2020-10-27 13:15:55 -0700

    login form talking to test-server.js

--- test-server.js
+++ test-server.js
@@ -4,20 +4,34 @@ require('dotenv').config();
 
 require('ejs');
 
+// ------------ DEPENDENCIES --------------
+
 const cors = require('cors');
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+
+// ------------- CONFIG -------------------
 
 const app = express();
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const client = new pg.Client(DATABASE_URL);
+app.set('view engine', 'ejs');
 
+// -------------- ROUTES ------------------
 app.get('/', adminLogin);
-app.post('/loggingIn', loggingIn);
+app.get('/login', loginPage);
+app.post('/logging-in', loggingIn);
 app.get('/admin', adminPage);
 
 
@@ -31,7 +45,18 @@ function adminLogin(req, res) {
 }
 
 function loggingIn(req, res) {
-  console.log(`REQUEST BODY ${req.body}`);
+  console.log(`TESTING REQUEST BODY ${JSON.stringify(req.body)}`);
+
+  // req.body.APIkey
+
+  // Verify that this key exists on JOTFORM
+  // save this key to localstorage as { APIkey : value }
+  // redirect to the admin page, which will then read that local storage key
+
+}
+
+function loginPage(req, res) {
+  res.render('pages/login');
 }
 
 function adminPage(req, res) {

f1c7469ec0918b6da93055baa23dd742e8223785
Mark Bell <skrambelled@gmail.com>
2020-10-27 21:50:35 -0700

    added session cookies

--- test-server.js
+++ test-server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 require('dotenv').config();
-
 require('ejs');
 
 // ------------ DEPENDENCIES --------------
@@ -12,6 +11,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 
 // ------------- CONFIG -------------------
@@ -19,8 +19,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -28,40 +28,58 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 app.set('view engine', 'ejs');
 
+// Global variables
+
+let jotFormKey = 0;
+
 // -------------- ROUTES ------------------
-app.get('/', adminLogin);
-app.get('/login', loginPage);
-app.post('/logging-in', loggingIn);
-app.get('/admin', adminPage);
+// app.get('/', (req, res) => res.redirect('/login'));
 
 
-function adminLogin(req, res) {
-  let APIkey = JSON.parse(localStorage.getItem('APIkey'));
-
-  if(APIkey)
-    res.redirect('/admin/');
+app.get('/', (req, res) => {
+  if(req.cookies && req.cookies.jotform)
+    res.redirect('/login/session');
   else
     res.redirect('/login');
-}
+});
 
-function loggingIn(req, res) {
-  console.log(`TESTING REQUEST BODY ${JSON.stringify(req.body)}`);
-
-  // req.body.APIkey
-
-  // Verify that this key exists on JOTFORM
-  // save this key to localstorage as { APIkey : value }
-  // redirect to the admin page, which will then read that local storage key
-
-}
+app.get('/login', loginPage);
+app.get('/login/session', loginSessionAuto);
+app.post('/login/session', loginSessionManual);
+app.get('/admin', adminPage);
 
 function loginPage(req, res) {
+  res.clearCookie('jotform');
   res.render('pages/login');
 }
 
-function adminPage(req, res) {
-  // res.render
+// called when a user inputs their API key from the /login page, and is redirected to /login/session via POST
+function loginSessionManual(req, res) {
+  loginSession(req, res, req.body.adminAPIkey);
+}
 
+// called when a user navigates to the / homepage, but is redirected to /login/session via GET
+function loginSessionAuto(req, res) {
+  loginSession(req, res, req.cookies.jotform)
+}
+
+function loginSession(req, res, key) {
+  let URL = `https://api.jotform.com/user?apiKey=${key}`;
+
+  superagent.get(URL)
+    .then( result => {
+      console.log(JSON.stringify(result));
+      res.cookie('jotform', key);
+      res.redirect('/admin');
+    })
+    .catch(err => {
+      res.clearCookie('jotform');
+      res.redirect('/login');
+    });
+}
+
+function adminPage(req, res) {
+  res.render('pages/admin', { key: req.cookies.jotform });
 }
 
 client.connect()

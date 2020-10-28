8e65440909ef520954ff83c93f3ee4ac30b9ac8d
Tahmina Ringer <tahmina.ringer@gmail.com>
2020-10-27 21:51:06 -0700

    linked chart to ejs and linked up form to ejs

--- test-server.js
+++ test-server.js
@@ -33,27 +33,47 @@ app.get('/', adminLogin);
 app.get('/login', loginPage);
 app.post('/logging-in', loggingIn);
 app.get('/admin', adminPage);
+app.get('/graph', graphPage);
+app.get('/survey', surveyPage);
 
 
 function adminLogin(req, res) {
-  let APIkey = JSON.parse(localStorage.getItem('APIkey'));
+  let APIkey = localStorage.getItem('APIkey');
 
   if(APIkey)
     res.redirect('/admin/');
   else
-    res.redirect('/login');
+    res.render('/login', {clearAPIkey: false});
 }
 
 function loggingIn(req, res) {
   console.log(`TESTING REQUEST BODY ${JSON.stringify(req.body)}`);
+  const adminKey = req.body.APIkey;
+  const url = `https://api.jotform.com/user?apiKey=${adminKey}`;
 
-  // req.body.APIkey
-
+  console.log(adminKey, 'console logging adminKey');
   // Verify that this key exists on JOTFORM
-  // save this key to localstorage as { APIkey : value }
-  // redirect to the admin page, which will then read that local storage key
-
+  superagent.get(url)
+    .then(result => {
+      if(result.body.responseCode === 200) {
+        // save this key to localstorage as { APIkey : value }
+        // localStorage.setItem('APIkey', 'adminKey');
+        // redirect to the admin page, which will then read that local storage key
+        res.render('/admin', {APIkey: adminKey});
+      } else {
+        res.render('/login', {clearAPIkey: true});
+        alert('Invalid key');
+      }
+    })
+    .catch(err => console.error(err));
 }
+
+
+
+
+
+
+
 
 function loginPage(req, res) {
   res.render('pages/login');
@@ -62,6 +82,14 @@ function loginPage(req, res) {
 function adminPage(req, res) {
   // res.render
 
+}
+
+function graphPage(req, res) {
+  res.render('pages/graph');
+}
+
+function surveyPage(req, res) {
+  res.render('pages/survey')
 }
 
 client.connect()
